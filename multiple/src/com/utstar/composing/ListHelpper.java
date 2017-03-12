df0aab7f033aaacd07c1ad985163bda6f4a75468
silver <Lenovo@Lenovo>
2017-03-13 00:04:03 +0800

    理解ListHelpper

--- multiple/src/com/utstar/composing/ListHelpper.java
+++ multiple/src/com/utstar/composing/ListHelpper.java
@@ -7,6 +7,9 @@ import java.util.List;
  * @author Lenovo
  * 向已有的安全类中添加功能的方法3：扩展功能，新建助手类。
  * 下面的做法是不安全的，因为用到了的不同的锁，所以保证不了原子性。（待验证，不是很理解）
+ * 
+ * 原意是给安全类list添加新的原子操作，即putIfAbsent，而list是pulbic域的，其他类也可以用到list，
+ * 如果有两个线程ListHelpper.list.add和ListHelpper.putIfAbsent就有可能出现错误
  * @param <E>
  */
 public class ListHelpper<E> {
